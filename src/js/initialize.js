8caaf41ff00ca49cec5f23324a64a81e0a7c53a9
madhaven <jitinjacobgigi@mca.ajce.in>
2022-10-16 22:34:20 +0530

    keeping scripts in the html cuz I'm old

--- src/js/initialize.js
+++
@@ -1,8 +0,0 @@
-towers = document.getElementsByClassName('tower')
-number_of_blocks = 4 // parseInt(prompt('Select the number of Hanoi blocks that you want'))
-data = [
-    [3, 2, 1, 0],
-    [],
-    []
-]
-updateUI()
